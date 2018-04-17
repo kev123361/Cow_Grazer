25e38e220f49385206b2a715a7208d32a08301a7
Kevin Tang <kev123361@gmail.com>
2018-04-17 18:51:07 -0400

    Initial Push

---
+++ Assets/Scripts/BasicSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicSlider : MonoBehaviour {
+
+    private Slider slider;
+    public GameObject grass;
+	// Use this for initialization
+	void Start () {
+        slider = this.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.X))
+        {
+            slider.value += .1f;
+        }
+        if (slider.value == 1f)
+        {
+            slider.value = 0f;
+            grass.transform.position = new Vector2(-13f, grass.transform.position.y);
+            gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/Scripts/GrassMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrassMove : MonoBehaviour {
+
+    public GameObject slider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -2f)
+        {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x + .6f, transform.position.y), Time.deltaTime);
+        } else
+        {
+            slider.SetActive(true);
+        }
+	}
+}
