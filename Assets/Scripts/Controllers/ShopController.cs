d5d151673e4cea9e99dbd7ac886d5bfd39273bb4
Kevin Tang <kev123361@gmail.com>
2018-04-22 15:51:59 -0400

    Basic Shop

--- Assets/Scripts/Controllers/ShopController.cs
+++ Assets/Scripts/Controllers/ShopController.cs
@@ -1,16 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour {
+    public GameController gc;
+    public Text moneyCount;
 
-	// Use this for initialization
-	void Start () {
+    public int[] upgradeCosts = { 100, 200, 500, 750, 1000 };
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        moneyCount.text = "$" + gc.money.ToString();
 	}
+
+    public void Purchase(string upgrade)
+    {
+        switch(upgrade)
+        {
+            case ("Eating Rate"):
+                if (gc.money >= upgradeCosts[gc.eatingRate])
+                {
+                    gc.money -= upgradeCosts[gc.eatingRate];
+                    gc.eatingRate += 1;
+                }
+                break;
+            case ("Hormones"):
+                if (gc.money >= upgradeCosts[gc.hormones])
+                {
+                    gc.money -= upgradeCosts[gc.hormones];
+                    gc.hormones += 1;
+                }
+                break;
+            case ("Equipment"):
+                if (gc.money >= upgradeCosts[gc.equipment])
+                {
+                    gc.money -= upgradeCosts[gc.equipment];
+                    gc.equipment += 1;
+                }
+                break;
+            case ("Field"):
+                if (gc.money >= upgradeCosts[gc.field])
+                {
+                    gc.money -= upgradeCosts[gc.field];
+                    gc.field += 1;
+                }
+                break;
+            case ("Spots"):
+                if (gc.money >= upgradeCosts[gc.spots])
+                {
+                    gc.money -= upgradeCosts[gc.spots];
+                    gc.spots += 1;
+                }
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/DisplayCost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCost : MonoBehaviour {
+    public GameController gc;
+    public ShopController sc;
+
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        text = GetComponentInChildren<Text>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        switch (gameObject.name) {
+            case ("EatingRateButton"):
+                text.text = "$" + sc.upgradeCosts[gc.eatingRate];
+                break;
+            case ("HormonesButton"):
+                text.text = "$" + sc.upgradeCosts[gc.hormones];
+                break;
+            case ("EquipmentButton"):
+                text.text = "$" + sc.upgradeCosts[gc.equipment];
+                break;
+            case ("FieldButton"):
+                text.text = "$" + sc.upgradeCosts[gc.field];
+                break;
+            case ("SpotsButton"):
+                text.text = "$" + sc.upgradeCosts[gc.spots];
+                break;
+        }
+	}
+}
