69f3f9e4c765a91460b6d681c99241428d69033f
Samuel Jenkins <sjenkins913@att.net>
2018-04-22 14:24:27 -0400

    main menu button

---
+++ Assets/Scripts/testButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class testButton : MonoBehaviour {
+
+	public void goToGame() {
+		Debug.Log ("You have clicked the button");
+		SceneManager.LoadScene("BasicLevel");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
