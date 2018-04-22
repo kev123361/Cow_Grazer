bc789ff23e4fd47dfc94ab14a9b3d48a4b23a7d0
Kevin Tang <kev123361@gmail.com>
2018-04-22 13:05:27 -0400

    Game Controller

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public int money;
+    public float frequency;
+    public float speed;
+
+    //Public variables for upgrade levels
+    public int eatingRate;
+    public int hormones;
+    public int equipment;
+    public int field;
+    public int spots;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
