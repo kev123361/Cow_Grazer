d10b27440872a85c2ceb1a0be652e9e8d6c068e4
Samuel Jenkins <sjenkins913@att.net>
2018-04-28 12:33:23 -0400

    Firing Kirby from Kirby works

--- Assets/Scripts/HeadTrack.cs
+++ Assets/Scripts/HeadTrack.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class HeadTrack : MonoBehaviour {
 
-	public fireBall fb;
+	public GameObject fb;
+	public Transform fbs;
 
 	// Use this for initialization
 	void Start () {
@@ -12,18 +13,19 @@ public class HeadTrack : MonoBehaviour {
 	}
 
 	void TaskOnClick() {
-		Debug.Log ("YOU JUST CLICKED THING");
-		Instantiate (fb, new Vector3(0, 0, 0), Quaternion.identity);
+		Debug.Log ("should instantiate thing wow");
+		Instantiate (fb, fbs.position, fbs.rotation);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 180;
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
 		if (Input.GetMouseButtonDown (0)) {
 			Debug.Log ("YOU JUST CLICKED THING");
+			TaskOnClick ();
 		}
 	}
 }
