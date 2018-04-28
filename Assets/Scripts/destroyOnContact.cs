481fe568b242f07de7c85b98459d78d8603e0306
Samuel Jenkins <sjenkins913@att.net>
2018-04-28 15:18:48 -0400

    sky event stuffs

---
+++ Assets/Scripts/destroyOnContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnContact : MonoBehaviour {
+
+	public GameController gc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("INTERACTING");
+		//if (other.tag == "projectile") {
+		//	gc.increaseMoney ();
+		//}
+		Destroy (other.gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/skyMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyMover : MonoBehaviour {
+
+	public float MoveSpeed = 0;
+
+	public float frequency = 0;  // Speed of sine movement
+	public float magnitude = 0;   // Size of sine movement
+	private Vector3 axis;
+
+	private Vector3 pos;
+
+	void Start () {
+		pos = transform.position;
+		//DestroyObject(gameObject, 1.0f);
+		axis = transform.up;  // May or may not be the axis you want
+
+	}
+
+	void Update () {
+		pos += transform.right * Time.deltaTime * MoveSpeed;
+		transform.position = pos + axis * Mathf.Sin (Time.time * frequency) * magnitude;
+	}
+}
